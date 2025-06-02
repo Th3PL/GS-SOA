fdc7ae053ef319c3bea0c2f231f39e1271c95802
Pedro Lucas <plucas2013@gmail.com>
2025-06-02 13:38:16 -0300

    Classes de entidade

---
+++ src/main/java/com/echohands/api/EchoID/domain/usuario/Usuario.java
@@ -0,0 +1,55 @@
+package com.echohands.api.EchoID.domain.usuario;
+
+import com.echohands.api.EchoID.domain.endereco.Endereco;
+
+public class Usuario {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    private Endereco endereco;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
