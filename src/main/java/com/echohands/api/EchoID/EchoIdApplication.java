c3daad43985eca2a64c2b2a2ca58e37a87b80019
Pedro Lucas <plucas2013@gmail.com>
2025-06-02 10:42:02 -0300

    first commit

---
+++ src/main/java/com/echohands/api/EchoID/EchoIdApplication.java
@@ -0,0 +1,13 @@
+package com.echohands.api.EchoID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echohands/api/EchoID/EchoIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echohands.api.EchoID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
